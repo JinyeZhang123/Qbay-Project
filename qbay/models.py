f85503f768f6d7ea940d47e6916e471123f9fd6c
Haoyu Li <17hl66@queensu.ca>
2021-10-10 04:36:28 -0400

    'register'

--- qbay/models.py
+++ qbay/models.py
@@ -1,12 +1,18 @@
+# model 
+
+import re
+
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
 
+from datetime import datetime
 
 '''
 This file defines data models and related business logics
 '''
 
-
+# define a global variable for checking valid email address
+regex = '^[a-z0-9]+[._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 db = SQLAlchemy(app)
 
 
@@ -18,6 +24,11 @@ class User(db.Model):
         primary_key=True)
     password = db.Column(
         db.String(120), nullable=False)
+    address = db.Column(
+        db.String(150), unique=False)
+    postcode = db.Column(
+        db.String(20), unique=False)
+    balance = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -27,6 +38,7 @@ class User(db.Model):
 db.create_all()
 
 
+#register function ----------------------------------
 def register(name, email, password):
     '''
     Register a new user
@@ -37,13 +49,48 @@ def register(name, email, password):
       Returns:
         True if registration succeeded otherwise False
     '''
+    # R1-1: Both the email and password cannot be empty.
+    # if empty input
+    if len(email) == 0 or len(password) == 0:
+        return False
+    # if not valid email format
+    if re.fullmatch(regex, email) is None:
+        return False
+    # password too short
+    if len(password) < 6:
+        return False
+    # password all lower
+    if password.islower():
+        return False
+    # password all upper
+    if password.isupper():
+        return False
+    # validate password (for special chars)
+    regexp = re.compile('[^0-9a-zA-Z]+')
+    if not regexp.search(password):
+        return False
+    # empty name
+    if len(name) < 0:
+        return False
+    # validate name (special chars)
+    # delete all spaces to check special chars only (check prefix or suffix later)
+    nametrimed = name.replace(' ', '')
+    if re.search("^[a-zA-Z0-9 ]*$", nametrimed) is None:
+        return False
+    # space allowed only if it is not as the prefix or suffix
+    if name.find(' ') == 0 or name.find(' ') == (len(name) - 1):
+        return False
+    # length limit of name
+    if len(name) <= 2 or len(name) >= 20:
+        return False
     # check if the email has been used:
     existed = User.query.filter_by(email=email).all()
+    # if by email not found
     if len(existed) > 0:
         return False
 
     # create a new user
-    user = User(username=name, email=email, password=password)
+    user = User(username=name, email=email, password=password, address=None, postcode=None, balance=100.0)
     # add it to the current database session
     db.session.add(user)
     # actually save the user object
@@ -51,17 +98,3 @@ def register(name, email, password):
 
     return True
 
-
-def login(email, password):
-    '''
-    Check login information
-      Parameters:
-        email (string):    user email
-        password (string): user password
-      Returns:
-        The user object if login succeeded otherwise None
-    '''
-    valids = User.query.filter_by(email=email, password=password).all()
-    if len(valids) != 1:
-        return None
-    return valids[0]
