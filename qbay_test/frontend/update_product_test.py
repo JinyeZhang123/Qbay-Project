728c331832f9cfc8f93eed7bd0d7f980aba00a0e
chen <18yz293@queensu.ca>
2021-11-06 00:13:21 -0400

    final check

--- qbay_test/frontend/update_product_test.py
+++ qbay_test/frontend/update_product_test.py
@@ -18,12 +18,12 @@ class FrontEndHomePageTest(BaseCase):
         and verify if the tickets are correctly listed.
         """
         self.open(base_url + '/register')
-            # fill email and password
+        # fill email and password
         self.type("#email", "shabi01@test.com")
         self.type("#name", "shabi001")
         self.type("#password", "1234567Aa!")
         self.type("#password2", "1234567Aa!")
-            # click enter button
+        # click enter button
         self.click('input[type="submit"]')
 
         # open login page
@@ -47,7 +47,8 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url + '/create_product')
 
         self.type("#title", "nanshoua01")
-        self.type("#description", "kylekylekylekylekylekylekylekylekylekylekylekylekylekylekyle")
+        self.type("#description", "kylekylekylekylekylekylekyleky\
+                                    lekylekylekylekylekylekylekyle")
         self.type("#price", "122.2")
         self.type("#date", "2021-10-30")
         self.type("#owner_email", "shabi01@test.com")
@@ -60,8 +61,6 @@ class FrontEndHomePageTest(BaseCase):
         self.type("#new_title", "nanshoua02")
         self.type("#description", "this iss a kyle kyle kyle kyle product!!!!")
         self.type("#price", "888.81")
-
-
 
         self.click('input[type="submit"]')
 
@@ -105,7 +104,8 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url + '/create_product')
 
         self.type("#title", "nanshoua03")
-        self.type("#description", "kylekylekylekylekylekylekylekylekylekylekylekylekylekylekyle")
+        self.type("#description", "kylekylekylekylekylekylekyleky\
+                                    lekylekylekylekylekylekylekyle")
         self.type("#price", "122.2")
         self.type("#date", "2021-10-30")
         self.type("#owner_email", "shabi02@test.com")
@@ -125,7 +125,6 @@ class FrontEndHomePageTest(BaseCase):
         self.assert_element("#message")
         self.assert_text("Product update failed.", "#message")
         # other available APIs
-
 
     def test_update_product_input_description_fail(self, *_):
         self.open(base_url + '/register')
@@ -158,7 +157,8 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url + '/create_product')
 
         self.type("#title", "nanshoua04")
-        self.type("#description", "kylekylekylekylekylekylekylekylekylekylekylekylekylekylekyle")
+        self.type("#description", "kylekylekylekylekylekylekyleky\
+                                    lekylekylekylekylekylekylekyle")
         self.type("#price", "122.2")
         self.type("#date", "2021-10-30")
         self.type("#owner_email", "shabi03@test.com")
@@ -209,7 +209,8 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url + '/create_product')
 
         self.type("#title", "nanshoua07")
-        self.type("#description", "kylekylekylekylekylekylekylekylekylekylekylekylekylekylekyle")
+        self.type("#description", "kylekylekylekylekylekylekyleky\
+                                    lekylekylekylekylekylekylekyle")
         self.type("#price", "122.2")
         self.type("#date", "2021-10-30")
         self.type("#owner_email", "shabi04@test.com")
@@ -260,7 +261,8 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url + '/create_product')
 
         self.type("#title", "nanshou01")
-        self.type("#description", "kylekylekylekylekylekylekylekylekylekylekylekylekylekylekyle")
+        self.type("#description", "kylekylekylekylekylekylekylekyl\
+                                    ekylekylekylekylekylekylekyle")
         self.type("#price", "122.2")
         self.type("#date", "2021-10-30")
         self.type("#owner_email", "shabi05@test.com")
@@ -279,7 +281,6 @@ class FrontEndHomePageTest(BaseCase):
         # test if the page loads correctly
         self.assert_element("#welcome-header")
         self.assert_text("Welcome shabi005 !", "#welcome-header")
-
 
     def test_update_product_output_fail(self, *_):
         self.open(base_url + '/register')
@@ -312,7 +313,8 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url + '/create_product')
 
         self.type("#title", "nanshou6")
-        self.type("#description", "kylekylekylekylekylekylekylekylekylekylekylekylekylekylekyle")
+        self.type("#description", "kylekylekylekylekylekylekyleky\
+                                    lekylekylekylekylekylekylekyle")
         self.type("#price", "122.2")
         self.type("#date", "2021-10-30")
         self.type("#owner_email", "shabi06@test.com")
@@ -331,6 +333,7 @@ class FrontEndHomePageTest(BaseCase):
         # test if the page loads correctly
         self.assert_element("#message")
         self.assert_text("Product update failed.", "#message")
+
     def test_update_product_functionality_success(self, *_):
 
         self.open(base_url + '/register')
@@ -363,7 +366,8 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url + '/create_product')
 
         self.type("#title", "haonanshoua01")
-        self.type("#description", "kylekylekylekylekylekylekylekylekylekylekylekylekylekylekyle")
+        self.type("#description", "kylekylekylekylekylekylekyleky\
+                                    lekylekylekylekylekylekylekyle")
         self.type("#price", "122.2")
         self.type("#date", "2021-10-30")
         self.type("#owner_email", "shabi07@test.com")
@@ -414,7 +418,8 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url + '/create_product')
 
         self.type("#title", "finally01")
-        self.type("#description", "kylekylekylekylekylekylekylekylekylekylekylekylekylekylekyle")
+        self.type("#description", "kylekylekylekylekylekylekyleky\
+                                    lekylekylekylekylekylekylekyle")
         self.type("#price", "122.2")
         self.type("#date", "2021-10-30")
         self.type("#owner_email", "shabi08@test.com")
